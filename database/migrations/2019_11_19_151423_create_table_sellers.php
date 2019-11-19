42b6a51fa0047f8f01cfbcd13bd3c3b6cb4caf91
Daniela Rodríguez <rodriguezh.daniela@gmail.com>
2019-11-19 10:19:33 -0500

    Create table sellers

---
+++ database/migrations/2019_11_19_151423_create_table_sellers.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableSellers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sellers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('type_id');
+            $table->integer('personal_id')->unique();
+            $table->string('name');
+            $table->string('last_name');
+            $table->string('address');
+            $table->integer('phone_number');
+            $table->string('e-mail')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sellers');
+    }
+}
