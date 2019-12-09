64a6cf2cad6d3c21741f89b32867555f6651a2db
Daniela Rodriguez <rodriguezh.daniela@gmail.com>
2019-12-09 08:38:47 -0500

    use factory and seeder

---
+++ database/factories/InvoiceFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\invoice;
+use Faker\Generator as Faker;
+
+$factory->define(invoice::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
